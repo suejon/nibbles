dffb13823c65c3281b044ce1c561935d3099d32d
Jono Sue <jonothan.sue@section6.nz>
2021-05-26 21:16:37 +1200

    remove unused dependencies

--- music/utils.py
+++ music/utils.py
@@ -3,9 +3,9 @@ import logging
 import string
 from pytube import YouTube
 
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-from nltk.corpus import stopwords
+# from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.feature_extraction.text import CountVectorizer
+# from nltk.corpus import stopwords
 
 from datetime import date
 
@@ -14,7 +14,7 @@ from rest_framework.utils import json
 
 from music.models import Track, Playlist
 
-stopwords = stopwords.words('english')
+# stopwords = stopwords.words('english')
 logger = logging.getLogger('Utils')
 
 def getproperty(section, key):
@@ -63,33 +63,33 @@ def getMatchingVideoId(query):
     print(f'Could not find any videos for query - {query}, response: {response}')
     return None
 
-def getBestMatch(items, text):
-    scores = []
+# def getBestMatch(items, text):
+#     scores = []
 
-    for i in items:
-        title = i['snippet']['title']
-        to_compare = [clean_text(text), clean_text(title)]
-        vectorizor = CountVectorizer().fit_transform(to_compare)
-        vectors = vectorizor.toarray()
-        cos_similarity = cosine_similarity(vectors[0].reshape(1,-1), vectors[1].reshape(1,-1))[0][0]
-        scores.append(cos_similarity)
+#     for i in items:
+#         title = i['snippet']['title']
+#         to_compare = [clean_text(text), clean_text(title)]
+#         vectorizor = CountVectorizer().fit_transform(to_compare)
+#         vectors = vectorizor.toarray()
+#         cos_similarity = cosine_similarity(vectors[0].reshape(1,-1), vectors[1].reshape(1,-1))[0][0]
+#         scores.append(cos_similarity)
 
-    index = scores.index(max(scores))
+#     index = scores.index(max(scores))
 
-    print('get best match')
-    print(f'scores: {scores}')
-    print(f'max: {max(scores)}')
-    print(f'max index: {scores.index(max(scores))}')
-    print(items)
-    print(items[index])
+#     print('get best match')
+#     print(f'scores: {scores}')
+#     print(f'max: {max(scores)}')
+#     print(f'max index: {scores.index(max(scores))}')
+#     print(items)
+#     print(items[index])
 
-    return items[index]
+#     return items[index]
 
-def clean_text(text):
-    text = ''.join([word for word in text if word not in string.punctuation])
-    text = text.lower()
-    text = ' '.join([word for word in text.split() if word not in stopwords])
-    return text
+# def clean_text(text):
+#     text = ''.join([word for word in text if word not in string.punctuation])
+#     text = text.lower()
+#     text = ' '.join([word for word in text.split() if word not in stopwords])
+#     return text
 
 def getArtistNames(arists_json):
     json = json.load(arists_json)
@@ -98,13 +98,19 @@ def getArtistNames(arists_json):
 def download_audio(video_id, destination_path):
     url = getproperty('youtube', 'public_url')
 
-    yt = YouTube(
-        f'{url}/watch?v={video_id}',
-        on_complete_callback=log_completed)
-    audio = yt.streams.filter(only_audio=True).first()
-    if audio is None:
+    try:
+        yt = YouTube(
+            f'{url}/watch?v={video_id}',
+            on_complete_callback=log_completed)
+        print(f' Downloading: {url}/watch?v={video_id}')
+        audio = yt.streams.filter(only_audio=True).first()
+        if audio is None:
+            return False
+        audio.download(destination_path)
+    except Exception as e:
+        print(e)
         return False
-    audio.download(destination_path)
+
     return True
 
 def log_completed(a,b):
